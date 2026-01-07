dfe2fee496999d2f48afd8054ab3ace95e75cf90
Vaibhav Ashok sonawane <ashoksonwane01@gmail.com>
2026-01-07 21:43:59 +0530

    First comit

---
+++ OOPs/oopsInPython.py
@@ -0,0 +1,19 @@
+#Class and Objects
+#Class:it is a bluebrint for creating an object
+
+#creating class
+class Student:
+    name="Karan"
+
+#creating Objects(instance)
+s1=Student()
+print(s1.name)
+
+#Eg 2:
+class car:
+    color="Blue"
+    brand="TATA"
+
+c1=car()
+print(c1.color)
+print(c1.brand)
